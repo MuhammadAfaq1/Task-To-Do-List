0370eab208fbf521008ee96db768336167cc5ab8
Muhammad Afaq Ali Saqib <103594434+MuhammadAfaq1@users.noreply.github.com>
2024-04-17 21:45:16 +0500

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Table.fxml"));
+			Scene scene = new Scene(root);
+	
+			stage.setTitle("TASK TODOLIST");
+			stage.setScene(scene);
+			stage.getIcons().add(new Image("application//todolist.jpg"));
+//			stage.setFullScreen(true);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
